9da157e7a9de1d8660587872b5014f6e286637aa
Efrén Gonzalez <0192704@up.edu.mx>
2019-11-22 01:49:22 -0600

    Funcion probabilidad

--- CKD.py
+++ CKD.py
@@ -1,5 +1,69 @@
 import pandas as pd
 
+bp = []
+dm = []
+htn = []
+clas = []
+male = []
+cmale = []
+maleB = []
+cmaleB = []
+female = []
+cfemale = []
+femaleB = []
+cfemaleB = []
+ignore = ['?']
+skip = ['age', 'sc', 'al', 'bp', 'dm', 'htn']
+htnP = 0
+htnF = 0
+dmP = 0
+dmF = 0
+bpP = [0, 0, 0, 0]
+bpF = [0, 0, 0, 0]
+gfrHBP = [0, 0, 0, 0, 0]
+gfrHBF = [0, 0, 0, 0, 0]
+gfrHNP = [0, 0, 0, 0, 0]
+gfrHNF = [0, 0, 0, 0, 0]
+gfrMBP = [0, 0, 0, 0, 0]
+gfrMBF = [0, 0, 0, 0, 0]
+gfrMNP = [0, 0, 0, 0, 0]
+gfrMNF = [0, 0, 0, 0, 0]
+
+
+"""
+sexo 0 = hombre, 1 = mujer
+raza 0 = no afrodescendiente, 1 = afrodescendiente 
+"""
+
+
+def probabilidad(gfrV, sexo, raza, bpV, dmV, htnV):
+    prob = 1.0
+    if sexo == 0:
+        if raza == 0:
+            prob *= (1.0-gfrHBP[gfrV])
+        else:
+            prob *= (1.0 - gfrHNP[gfrV])
+    else:
+        if raza == 0:
+            prob *= (1.0-gfrMBP[gfrV])
+        else:
+            prob *= (1.0 - gfrMNP[gfrV])
+    print(prob)
+    prob *= (1.0 - bpP[bpV])
+    print(bpP[bpV], prob)
+    if dmV == 1:
+        prob *= (1.0 - 1.0)
+    else:
+        prob *= (1.0 - dmP)
+    print(dmP, prob)
+    if htnV == 1:
+        prob *= (1.0 - 1.0)
+    else:
+        prob *= (1.0 - htnP)
+    print(htnP, prob)
+    return 1.0 - prob
+
+
 def sum(lista):
     suma = 0
     for i in range(len(lista)):
@@ -51,20 +115,6 @@ def eGFR(sc, k, a, age, sex, race):
            * (0.993**age) * sex * race
 
 
-bp = []
-dm = []
-htn = []
-clas = []
-male = []
-cmale = []
-maleB = []
-cmaleB = []
-female = []
-cfemale = []
-femaleB = []
-cfemaleB = []
-ignore = ['?']
-skip = ['age', 'sc', 'al', 'bp', 'dm', 'htn']
 file = pd.read_csv('renal.csv')
 for i in range(1, file['age'].size):
     _continue = False
@@ -101,20 +151,6 @@ for i in range(1, file['age'].size):
         clas.append(1)
     else:
         clas.append(0)
-htnP = 0
-htnF = 0
-dmP = 0
-dmF = 0
-bpP = [0, 0, 0, 0]
-bpF = [0, 0, 0, 0]
-gfrHBP = [0, 0, 0, 0, 0]
-gfrHBF = [0, 0, 0, 0, 0]
-gfrHNP = [0, 0, 0, 0, 0]
-gfrHNF = [0, 0, 0, 0, 0]
-gfrMBP = [0, 0, 0, 0, 0]
-gfrMBF = [0, 0, 0, 0, 0]
-gfrMNP = [0, 0, 0, 0, 0]
-gfrMNF = [0, 0, 0, 0, 0]
 for i in range(len(htn)):
     if htn[i] == 0:
         htnF += 1
@@ -135,11 +171,22 @@ htnP /= htnF
 dmP /= dmF
 for i in range(len(bpP)):
     bpP[i] /= bpF[i]
-print(gfrHBP, gfrHBF, sum(gfrHBF))
-print(gfrHNP, gfrHNF, sum(gfrHNF))
-print(gfrMBP, gfrMBF, sum(gfrMBF))
-print(gfrMNP, gfrMNF, sum(gfrMNF))
-
+"""
+print("hombres no afrodescendientes")
+for i in range(len(clas)):
+    print(i, clas[i], probabilidad(cmale[i], 0, 0, bp[i], dm[i], htn[i]))
+print("hombres afrodescendientes")
+for i in range(len(clas)):
+    print(i, clas[i], probabilidad(cmaleB[i], 0, 1, bp[i], dm[i], htn[i]))
+print("mujeres no afrodescendientes")
+for i in range(len(clas)):
+    print(i, clas[i], probabilidad(cfemale[i], 1, 0, bp[i], dm[i], htn[i]))
+print("mujeres afrodescendientes")
+for i in range(len(clas)):
+    print(i, clas[i], probabilidad(cfemaleB[i], 1, 1, bp[i], dm[i], htn[i]))
+"""
+print(gfrMBP, bpP, dmP, htnP)
+print(probabilidad(0, 1, 0, 0, 0, 0))
 
 """
     Fuente https://www.kidney.org/content/ckd-epi-creatinine-equation-2009
