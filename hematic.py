86d974044295234d65a9d4c136a41d27e95cfc29
PrettyBoyHelios <helios@polispay.org>
2019-11-21 21:19:08 -0600

    sad

---
+++ hematic.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+
+# Cleans blood pressure
+def clean_bp(d):
+    """
+    Assumes systolic pressure using
+    :param d: dataset object. to keep a copy.
+    :param bp: blood pressure value.
+    :return: modified record.
+    """
+
+    for x in data:
+        print(x[])
+
+data = pd.read_csv("renal.csv")
+data = clean_bp(data)
+
