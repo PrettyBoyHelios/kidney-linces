396c65e556c59b2874276bc641ed6ca3a106d40f
Helios <helios@polispay.org>
2019-11-21 23:36:23 -0600

    PCA

--- svm.py
+++ svm.py
@@ -1,13 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn import svm
+
+data = pd.read_csv("datasets/lifestyle.csv")
+data = data.sample(frac=1).reset_index(drop=True)
+x_columns = ["bp", "grav", "htn", "dm", "cad", "appet", "pe", "ane"]
+x = data[x_columns]
+y = data['class']
+for col in data:
+    print(col)
+print(x)
+print(x_columns)
 
 
-X = np.array([[-1, 1, 6], [-2, 1, -2], [10, -2, -4], [1, 1, 4], [2, 1, 8], [3, 2, 10]])
-y = np.array([1, 1, 3, 1, 2, 2])
-lda = LinearDiscriminantAnalysis()
+
+X = np.array(x)
+y = np.array(y)
+lda = PCA(n_components=2)
 X_lda = lda.fit_transform(X, y)
 print("ratio: ", lda.explained_variance_ratio_)
+print(lda.score)
 
 plt.xlabel('LD1')
 plt.ylabel('LD2')
@@ -17,7 +32,9 @@ plt.scatter(
     c=y,
     cmap='rainbow',
     alpha=0.7,
-    edgecolors='b'
+    edgecolors='b',
+    
 )
+
 plt.show()
-print(lda.predict([[4, 1, 6]]))
+#print(lda.predict([[4, 1, 6]]))
